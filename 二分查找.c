7be0dfa6c19135a5d7b234584b6e9948f0ca8916
HKishere <48638533+HKishere@users.noreply.github.com>
2019-03-26 23:16:42 +0800

    Create 二分查找.c

---
+++ 二分查找.c
@@ -0,0 +1,31 @@
+#include <stdio.h> 
+#include <stdlib.h>
+int search(int a[],int left,int right,int find){
+    int mid;
+    while(left<=right){
+        mid=(right-left)/ 2;
+        if(a[mid]>find){
+            right=mid-1;
+        }
+        else if(a[mid]<find){
+            left=mid+1;
+        }
+        else if(a[mid]==find){
+            return mid;
+        }
+        else{
+            return -1;
+        }
+    }
+}
+int main(){
+    int a[10]={1,4,6,8,12,23,34,45,56,78};
+    int find,i;
+    int left=0;
+    int right=9;
+    scanf ("%d",&find);
+    i=search(a,left,right,find);
+    printf("%d",i);
+    system("pause");
+    return 0;
+}
