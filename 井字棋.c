d0a257d85c484cc63d1be1b0de6a2b7264056b95
HKishere <939132879@qq.com>
2019-04-04 20:46:45 +0800

    完成井字棋最终版

--- 井字棋.c
+++ 井字棋.c
@@ -7,26 +7,26 @@
 #define MAX_COL 3;
 char ches[3][3];
 int menu() {
-		while (1) {
-			int choose;
-			printf("******************\n");
-			printf("***** 1.play *****\n");
-			printf("***** 2.exit *****\n");
-			printf("******************\n");
-			printf("please enter your choose:");
-			scanf("%d", &choose);
-			if (choose == 2) {
-				printf("goodbye!\n");
-				system("pause");
-				return  1;
-			}
-			else if (choose != 1) {
-				printf("illegal input ! please enter again:\n");
-			}
-			else {
-				break;
-			}
+	while (1) {
+		int choose;
+		printf("******************\n");
+		printf("***** 1.play *****\n");
+		printf("***** 2.exit *****\n");
+		printf("******************\n");
+		printf("please enter your choose:");
+		scanf("%d", &choose);
+		if (choose == 2) {
+			printf("goodbye!\n");
+			system("pause");
+			return  1;
 		}
+		else if (choose != 1) {
+			printf("illegal input ! please enter again:\n");
+		}
+		else {
+			break;
+		}
+	}
 	return 0;
 }
 int printboard() {
@@ -45,13 +45,13 @@ int playermove() {
 		getchar();/*Çå³ýÊäÈë»º³åÇø*/
 		printf("please enter the coordinate you want to place your chess,use ','to separate :\n ");
 		scanf("%d,%d", &row, &col);
-		if (row > 3 || row < 0 || col>3 || col < 0) {
+		if (row >3 || row < 0 || col>3 || col < 0) {
 			printf("please enter the correct coordinate!\n");
 			continue;
 		}
 		if (ches[row - 1][col - 1] == '\0') {
 			ches[row - 1][col - 1] = 'X';
-			printf("player place the chess in'%d,%d\n'", row, col); 
+			printf("player place the chess in'%d,%d'\n", row, col);
 			break;
 		}
 		else {
@@ -63,23 +63,35 @@ int playermove() {
 	return 0;
 }
 int winner_checker() {
-	char playerwin[3] = { 'X','X','X' };
-	char computerwin[3] = { 'O','O','O' };
-	if (strcmp(ches[0],playerwin)==0||strcmp(ches[1],playerwin)==0||strcmp(ches[2],playerwin)==0||(ches[0][0]=='X'&&ches[1][0]=='X'&&ches[2][0])||ches[0][1]=='X'&&ches[1][1]=='X'&&ches[2][1])||(ches[0][2]=='X'&&ches[1][2]=='X'&&ches[2][2])){
-		return 0;//0 mean player win
+
+	if ((ches[0][0] == 'X'&&ches[0][1] == 'X'&&ches[0][2] == 'X') ||
+		(ches[1][0] == 'X'&&ches[1][1] == 'X'&&ches[2][2] == 'X') ||
+		(ches[2][0] == 'X'&&ches[2][1] == 'X'&&ches[2][2] == 'X') ||
+		(ches[0][0]== 'X'&&ches[1][0]== 'X'&&ches[2][0] == 'X')||
+		(ches[0][1] == 'X'&&ches[1][1] == 'X'&&ches[2][1] == 'X') ||
+		( ches[0][2] == 'X'&&ches[1][2] == 'X'&&ches[2][2] == 'X')){
+	return 0;//0 mean player win
 	}
-	else if (ches[0][0]=='X'&&ches[1][1]=='X'&&ches[2][2]=='X'){
+	else if ((ches[0][0] == 'X'&&ches[1][1] == 'X'&&ches[2][2] == 'X')||
+				(ches[0][2] == 'X'&&ches[1][1] == 'X'&&ches[2][0] == 'X')) {
 		return 0;
 	}
-	if (strcmp(ches[0],computerwin)==0||strcmp(ches[1],computerwin)==0||strcmp(ches[2],computerwin)==0)||(ches[0][0]=='O'&&ches[1][0]=='O'&&ches[2][0])||ches[0][1]=='O'&&ches[1][1]=='O'&&ches[2][1])||(ches[0][2]=='O'&&ches[1][2]=='O'&&ches[2][2]){
+	if ((ches[0][0] == 'O'&&ches[0][1] == 'O'&&ches[0][2]=='O') ||
+		(ches[1][0] == 'O'&&ches[1][1] == 'O'&&ches[2][2]=='O') ||
+		(ches[2][0] == 'O'&&ches[2][1] == 'O'&&ches[2][2]=='O') ||
+		(ches[0][0] == 'O'&&ches[1][0] == 'O'&&ches[2][0] == 'O') ||
+		(ches[0][1] == 'O'&&ches[1][1] == 'O'&&ches[2][1] == 'O') ||
+		(ches[0][2] == 'O'&&ches[1][2] == 'O'&&ches[2][2] == 'O')){
 		return 1;// 1 means computer win
-	}else if (ches[0][0] == 'O'&&ches[1][1] == 'O'&&ches[2][2] == 'O'){
+	}
+	else if ((ches[0][0] == 'O'&&ches[1][1] == 'O'&&ches[2][2] == 'O')||
+				(ches[0][2] == 'O'&&ches[1][1] == 'O'&&ches[2][0] == 'O')){
 		return 1;
 	}
 	return 2;
 }
 int computermove() {
-	/*srand((unsigned int)time(0));*/
+	srand((unsigned int)time(0));
 	while (1) {
 		int col = rand() % 3 + 1;
 		int row = rand() % 3 + 1;
@@ -90,40 +102,48 @@ int computermove() {
 		}
 		else {
 			continue;
-		}	
+		}
 	}
 	printboard();
 	return 0;
 }
+int full_check() {
+	int row, col;
+	for (row = 0; row < 3; ++row) {
+		for (col =0; col < 3; ++col) {
+			if (ches[row][col] == '\0') {
+				return 0;
+			}
+		}
+	}
+	return 1;
+}
 int game() {
-	int win=100;
-	while(1){
-		printboard();
+	int win = 100;
+	int full = 2;
+	printboard();
+	while (1) {
 		playermove();
-		win=winner_checker();
-		if(win==1){
-			printf("computer win!\n");
+		win = winner_checker();
+		if (win == 0) {
+			printf("player win\n");
 			break;
 		}
-		else if(win==0){
-		 	printf("player win\n");
-		 	break;
+		if (full_check() == 1) {
+			printf("draw!\n");
+			break;
 		}
 		computermove();
-		winner_checker();
-		if(win==1){
-			printf("computer win!");
+		win=winner_checker();
+		if (win == 1) {
+			printf("computer win!\n");
 			break;
-		}
-		else if(win==0){
-		 	printf("player win");
-		 	break;
 		}
 	}
 	return 0;
 }
-int main (){
-	if (menu()==1){
+int main() {
+	if (menu() == 1) {
 		return 0;
 	}
 	game();
