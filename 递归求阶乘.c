857d110dabeef9c6d307c5e65a118c013290d42c
HKishere <939132879@qq.com>
2019-04-09 14:47:46 +0800

    修改了代码使得结果正确

--- 递归求阶乘.c
+++ 递归求阶乘.c
@@ -1,7 +1,8 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h> 
 #include <stdlib.h>
 int fun(int a){
-    if (a=1){
+    if (a==1){
         return 1;
     }
     else{
@@ -11,7 +12,7 @@ int fun(int a){
 int main(){
     int a;
     scanf("%d",&a);
-    printf("%d",fun(a));
+    printf("%d\n",fun(a));
     system("pause");
     return 0;
 }
