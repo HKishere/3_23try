30a2b1aaef96f531faadd48ea0e7c63d57e81ce1
HKishere <939132879@qq.com>
2019-04-17 20:18:40 +0800

    Create 左旋字符串.c

---
+++ 左旋字符串.c
@@ -0,0 +1,23 @@
+#define  _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+char* leftroll(char* source ,unsigned int i) {
+	char temp;
+	i %= len;
+	for (; i != 0; --i) {
+		temp = source[len];
+		for (unsigned int n = len; n > 0; --n) {
+			source[n] = source[n - 1];
+		}
+		source[0] = temp;
+	}
+	return source;
+}
+int main (){
+	char str[] = "kishere";
+	leftroll(str, 90);
+	printf("%s\n", str);
+	system("pause");
+	return 0;
+}
