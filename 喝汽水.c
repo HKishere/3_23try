df8280dd4076e1d76bed31c5bb2aa4336c1a4c36
HKishere <939132879@qq.com>
2019-04-23 16:12:49 +0800

    Create 喝汽水.c

---
+++ 喝汽水.c
@@ -0,0 +1,18 @@
+#define  _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<stdlib.h>
+#define  START_MONEY 20
+int main (){
+	int bottle = 0;
+	int alreadydrink = 0;
+	int money = START_MONEY;
+	while (money > 0) {
+		alreadydrink += money;
+		bottle += money;
+		money = bottle / 2;
+		bottle %= 2;
+	}
+	printf("you can drink %d bottle of drink\n", alreadydrink);
+	system("pause");
+	return 0;
+}
