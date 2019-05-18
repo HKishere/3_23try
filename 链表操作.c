64dbed9854f7dea4f5966836af255d1da5cf3989
HKishere <939132879@qq.com>
2019-05-18 23:14:03 +0800

    Update 链表操作.c

--- 链表操作.c
+++ 链表操作.c
@@ -1,6 +1,7 @@
 #define  _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
 #include<stdlib.h>
+#define MAX_SIZE 10
 
 typedef int SDataType; 
 
@@ -19,17 +20,24 @@ typedef struct SList
 
 // 链表的初始化 
 void SListInit(SList* s){
-	s->_pHead = (PNode)malloc(sizeof (PSList));
+	s->_pHead = NULL;
 	return;
 }
 
 // 在链表s最后一个节点后插入值为data的节点 
 void SListPushBack(SList* s, SDataType data) {
 	PNode p = s->_pHead;
-	while (p->_pNext) {
+	if (p == NULL) {
+		PNode newnode = (PNode)malloc(sizeof(Node));
+		s->_pHead = newnode;
+		newnode->_data = data;
+		newnode->_pNext = NULL;
+		return;
+	}
+	while (p->_pNext != NULL) {
 		p = p->_pNext;
 	}
-	PNode newnode = (PNode)malloc(sizeof(PNode));
+	PNode newnode = (PNode)malloc(sizeof(Node));
 	p->_pNext = newnode;
 	newnode -> _data = data;
 	newnode -> _pNext = NULL;
@@ -39,7 +47,7 @@ void SListPushBack(SList* s, SDataType data) {
 // 删除链表s最后一个节点 
 void SListPopBack(SList* s) {
 	PNode p = s->_pHead;
-	while (p->_pNext->_pNext) {
+	while (p->_pNext->_pNext != NULL) {
 		p = p->_pNext;
 	}
 	free(p->_pNext);
@@ -49,7 +57,7 @@ void SListPopBack(SList* s) {
 
 // 在链表s第一个节点前插入值为data的节点 
 void SListPushFront(SList* s, SDataType data) {
-	PNode newnode = (PNode)malloc(sizeof(PNode));
+	PNode newnode = (PNode)malloc(sizeof(Node));
 	newnode->_data = data;
 	newnode->_pNext = s->_pHead;
 	s->_pHead = newnode;
@@ -69,11 +77,12 @@ void SListInsert(PNode pos, SDataType data) {
 	newnode->_pNext = pos->_pNext;
 	newnode->_data = data;
 	pos->_pNext = newnode;
+	return;
 }
 
 // 删除链表s中pos位置的节点 
 void SListErase(SList* s, PNode pos){
-	PNode p = (PNode)malloc(sizeof(Node));
+	PNode p = s->_pHead;
 	while (p->_pNext != pos) {
 		p = p->_pNext;
 	}
@@ -86,11 +95,11 @@ void SListErase(SList* s, PNode pos){
 PNode SListFind(SList* s, SDataType data) {
 	PNode p = s->_pHead;
 	do {
-		if (p->_data = data){
+		if (p->_data == data){
 			break;
 		}
 		p = p->_pNext;
-	} while (p->_pNext == NULL);
+	} while (p->_pNext != NULL);
 	return p;
 }
 // 获取链表中有效节点的个数 
@@ -122,19 +131,53 @@ void SListClear(SList* s) {
 }
 
 // 销毁链表 
-void SListDestroy(SList* s) {
+void SListDestroy(PSList s) {
 	PNode p = s->_pHead;
-	PNode ptr = p->_pNext;
-	while (p) {
-		free(p);
-		p = ptr;
-		ptr = p->_pNext;
+	PNode ptr = p;
+	while (ptr) {
+		p = ptr->_pNext;
+		free(ptr);
+		ptr = p;
 	}
 	free(s);
 }
+void SlistPrint(PSList p) {
+	PNode prt = p->_pHead;
+	while (prt != NULL){
+		printf("%u ", prt->_data);
+		prt = prt->_pNext;
+	}
+	printf("\n");
+	return;
+}
+void testslist(PSList p) {
+	SListInit(p);
+	SListPushBack(p, 1);
+	SListPushBack(p, 2);
+	SListPushBack(p, 3);
+	SListPushBack(p, 4);
+	SListPushBack(p, 5);
+	SlistPrint(p);
+	SListPopBack(p);
+	SlistPrint(p);
+	SListPushFront(p, 0);
+	SlistPrint(p);
+	SListPopFront(p);
+	SlistPrint(p);
+	printf("size : %d\n", SListSize(p));
+	PNode prt = SListFind(p, 3);
+	SListErase(p , prt);
+	SlistPrint(p);
+	prt = SListFind(p, 2);
+	SListInsert(prt, 77);
+	SlistPrint(p);
+	return;
+}
 
 int main (){
-	
+	PSList myslist = (PSList)malloc(sizeof(PSList));
+	testslist(myslist);
+	SListDestroy(myslist);
 	system("pause");
 	return 0;
 }
