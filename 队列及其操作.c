e97935a7e506dae830f64fd0bfb1a9b95330c841
HKishere <939132879@qq.com>
2019-05-19 14:54:43 +0800

    增加了队列测试的代码

--- 队列及其操作.c
+++ 队列及其操作.c
@@ -1,8 +1,22 @@
 #define  _CRT_SECURE_NO_WARNINGS
-#include<stdio.h>
-#include<stdlib.h>
+#include "queue.h"
 int main (){
-	
+	Queue* qu = (Queue*)malloc(sizeof(Queue));
+	QueueInit(qu);
+	QueuePush(qu, 1);
+	QueuePush(qu, 2);
+	QueuePush(qu, 3);
+	QueuePush(qu, 4);
+	QueuePush(qu, 5);
+	QueuePrint(qu);
+	QueuePop(qu);
+	QueuePrint(qu);
+	QueuePush(qu, 6);
+	QueuePrint(qu);
+	printf("size of queue:%d\n", QueueSize(qu));
+	printf("first data is : %d\n", QueueFront(qu));
+	printf("last data is : %d\n", QueueBack(qu));
+	QueueDestroy(qu);
 	system("pause");
 	return 0;
 }
