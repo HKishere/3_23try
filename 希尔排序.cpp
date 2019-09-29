e02ea0ebc1f99b4b224da80b9750bafe4a53e88b
HKishere <939132879@qq.com>
2019-09-29 17:41:04 +0800

    upload code

--- 希尔排序.cpp
+++ 希尔排序.cpp
@@ -1,20 +1,27 @@
-#include<iostream>
-#include <vector>
+#include <iostream>
 using namespace std;
 
-vector<int> arr;
-
-void shell_sort(vector<int> arr, int length) {
-	int h = 1;
-	while (h < length / 3) {
-		h = 3 * h + 1;
+int Num2[]{ 3,2,5,6,4,1,9,8,8,10,1 };
+void ShellSort(int a[], int n) {
+	int step = 1;
+	while (step <= n / 3) {
+		step = 3 * step + 1;
 	}
-	while (h >= 1){
-		for (int i = h; i < length; ++i) {
-			for (int j = i; j >= h&&arr[j] < arr[j - h]; j -= h) {
-				swap(arr[j], arr[j - h]);
+	while (step >= 1) {
+		for (int i = step; i < n; ++i) {
+			for (int j = i; j >= step&&a[j] < a[j - step]; j -= step) {
+				std::swap(a[j], a[j - step]);
 			}
 		}
-		h = h / 3;
+		step = step / 3;
 	}
+}
+int main3() {
+	ShellSort(Num2, 11);
+	for (int i = 0; i < 11; ++i) {
+		cout << Num2[i] << " ";
+	}
+	cout << endl;
+	system("pause");
+	return 0;
 }
---
+++ 快速排序.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+
+int Num3[]{ 3,2,5,6,4,1,9,8,8,10,1 };
+void QuickSort(int a[], int n) {
+	if (n <= 1) {
+		return;
+	}
+	int mark = 0;
+	for (int i = 1; i < n; ++i) {
+		if (a[i] < a[0]) {
+			mark++;
+			std::swap(a[mark], a[i]);
+		}
+	}
+	std::swap(a[mark], a[0]);
+	QuickSort(a, mark);
+	QuickSort(&a[mark + 1], n - mark - 1);
+}
+void Print(int a[]) {
+	for (int i = 0; i < 11; ++i) {
+		cout << a[i] << " ";
+	}
+	cout << endl;
+}
+int main() {
+	QuickSort(Num3, 11);
+	Print(Num3);
+	system("pause");
+	return 0;
+}
